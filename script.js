e6ad6fdc2cfe9080fb4249ef2fdbe9e02ae79a39
ranjith1807 <v.ranjith9095490240@gmail.com>
2025-12-31 12:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let time=document.getElementById('timer');
+let currdate=new Date();
+
+setInterval(
+	function(){
+		currdate=new Date();
+		time.innerHTML=currdate.toLocaleString();
+	},1000
+)
